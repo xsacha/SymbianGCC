f7bdb43468b32918da66d163f0520125d41fbc38
Sacha <xsacha@gmail.com>
2014-06-06 15:49:13 +1000

    Upgrade to GCC 4.6.4

--- gcc/config/mips/mips.c
+++ gcc/config/mips/mips.c
@@ -16627,7 +16627,10 @@ mips_reorg (void)
     mips_rest_of_frame_header_opt ();
 
   if (optimize > 0 && flag_delayed_branch)
-    dbr_schedule (get_insns ());
+    {
+      cleanup_barriers ();
+      dbr_schedule (get_insns ());
+    }
   mips_reorg_process_insns ();
   if (!TARGET_MIPS16
       && TARGET_EXPLICIT_RELOCS
